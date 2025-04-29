f0df1ed37f49e59084682407be04a6c012a5a7a3
Hemant <guptahemant350@gmail.com>
2025-04-29 21:03:52 +0530

    Main:Routing

--- Myntra-Clone/src/main.jsx
+++ Myntra-Clone/src/main.jsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import "bootstrap/dist/css/bootstrap.min.css"
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./routes/App.jsx";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Bag from "./routes/Bag.jsx";
+import Home from "./routes/Home.jsx";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/bag", element: <Bag /> },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
