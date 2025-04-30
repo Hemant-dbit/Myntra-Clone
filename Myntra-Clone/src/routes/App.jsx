cba9f4f564b194d567b349adede41a3abf568eb7
Hemant <guptahemant350@gmail.com>
2025-04-30 22:25:34 +0530

    App: Added Fetch Items

--- Myntra-Clone/src/routes/App.jsx
+++ Myntra-Clone/src/routes/App.jsx
@@ -2,12 +2,13 @@ import Header from "../components/Header";
 import Footer from "../components/Footer";
 import { Outlet } from "react-router-dom";
 import "../index.css";
+import FetchItems from "../components/fetchItems";
 
 function App() {
-  
   return (
     <>
       <Header />
+      <FetchItems />
       <Outlet />
       <Footer />
     </>
