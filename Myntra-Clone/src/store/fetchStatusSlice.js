4a31ddf8541482b7703a567005b96e11222af8b3
Hemant <guptahemant350@gmail.com>
2025-05-02 22:27:15 +0530

    FetchStatus: fetch states

---
+++ Myntra-Clone/src/store/fetchStatusSlice.js
@@ -0,0 +1,23 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const fetchStatusSlice = createSlice({
+  name: "fetchStatus",
+  initialState: {
+    currentlyfetching: false,
+    fetchDone: false,
+  },
+  reducers: {
+    markFetchDone: (state) => {
+      state.fetchDone = true;
+    },
+    markFetchingStarted: (state) => {
+      state.currentlyfetching = true; 
+    },
+    markFetchingFinished: (state) => {
+      state.currentlyfetching = false;
+    },
+  },
+});
+
+export const fetchStatusActions = fetchStatusSlice.actions;
+export default fetchStatusSlice;
